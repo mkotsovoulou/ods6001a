db2e27ba4f18f13a31d0ec9f5bc1e4431001674a
Maira Kotsovoulou <mkotsovoulou@acg.edu>
2022-02-26 13:26:11 +0200

    changed tests

--- assignments/assignment1/__init__.py
+++ assignments/assignment1/__init__.py
@@ -3,12 +3,12 @@ import check50  # import the check50 module
 
 @check50.check()  # tag the function below as check50 check
 def exists():  # the name of the check
-    """Is your file saved as problem1.py"""  # this is what you will see when running check50
+    """Is your file saved as assignment1.py ?"""  # this is what you will see when running check50
     check50.exists("assignment1.py")  # the actual check
 
 
 @check50.check(exists)  # only run this check if the exists check has passed
-def prints2():
-    """Does the program return 96.25 when the input is 35 and 2.75? """
+def test1():
+    """Does the program return 96.25 when the input is 35 and 2.75 respectively? """
     check50.run("python3 assignment1.py").stdin(
         "35").stdin("2.75").stdout("96.25\n").exit(0)
--- labs/lab1_1/__init__.py
+++ labs/lab1_1/__init__.py
@@ -1,13 +1,13 @@
 import check50  # import the check50 module
 
-
 @check50.check()  # tag the function below as check50 check
 def exists():  # the name of the check
-    """Is your file saved as types.py"""  # this is what you will see when running check50
-    check50.exists("types.py")  # the actual check
+    """Is your file saved as lab1_1.py?"""  # this is what you will see when running check50
+    check50.exists("lab1_1.py")  # the actual check
 
 
 @check50.check(exists)  # only run this check if the exists check has passed
-def prints2():
+def test1():
     """Does it print "2" when you input 1 for x and 1 for y ? """
-    check50.run("python3 types.py").stdin("1").stdin("1").stdout("2.0\n").exit(0)
+    check50.run("python3 lab1_1.py").stdin(
+        "1").stdin("1").stdout("2.0\n").exit(0)
