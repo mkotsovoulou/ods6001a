753a12e2bb4a7455d218f6ede36c7dd4ed08d5af
Maira Kotsovoulou <mkotsovoulou@acg.edu>
2022-03-05 14:18:34 +0200

    renamed assignment1

--- assignments/assignment2/assignment2.py
+++ assignments/assignment2/assignment2.py
@@ -1,4 +1,11 @@
+# Get input from the user for hours worked and rate per hour
 hours = int(input("Enter hours worked: "))
 ratePerHour = float(input("Enter rate per hour: "))
 
+# if hours worked are greater than 40 
+# calculate the pay with a rate 1.5 times the hourly rate for all hours worked above 40
 
+
+
+
+# print the result
