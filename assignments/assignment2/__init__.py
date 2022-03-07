b9a90066d7a08f71db6055b88cf2232f1a371f8b
Maira Kotsovoulou <mkotsovoulou@acg.edu>
2022-03-07 21:01:00 +0200

    ass2

--- assignments/assignment2/__init__.py
+++ assignments/assignment2/__init__.py
@@ -2,7 +2,7 @@ import check50  # import the check50 module
 
 @check50.check()  # tag the function below as check50 check
 def exists():  # the name of the check
-    """Is your file saved as problem2.py"""  # this is what you will see when running check50
+    """Is your file saved as assignment2.py"""  # this is what you will see when running check50
     check50.exists("assignment2.py")  # the actual check
 
 
