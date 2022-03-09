1db6cd3fbc851c313c559f29162f3f00ea13e3b3
Maira Kotsovoulou <mkotsovoulou@acg.edu>
2022-03-09 21:47:24 +0200

    lab2_2 added tests

--- labs/lab2_2/lab2_2.py
+++ labs/lab2_2/lab2_2.py
@@ -10,10 +10,23 @@ def add():
     return total
 
 
-# Ask the user how many numbers they want to add and assign it to a variable called "numbers"
-# (Validate user input and quit if not a number)
+# This is the main program function
+def main():
+    sum = 0
+    # Ask the user how many numbers they want to add and assign it to a variable called "numbers"
+    # (Validate user input and quit if not a number)
 
-# Call the add function by passign numbers as an argument. Assign the value returned from the function to the sum variable
-sum = 0
 
-# print the sum
+    # Call the add function by passign numbers as an argument. Assign the value returned from the function to the sum variable
+    
+
+    # print the sum
+
+
+
+
+#DO NOT MODIFY THIS CODE
+if __name__ == '__main__':
+    main()
+
+
