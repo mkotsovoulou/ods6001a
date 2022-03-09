3c54d15c6b2f69cdca1aecc94dad588b51bf0f06
Maira Kotsovoulou <mkotsovoulou@acg.edu>
2022-03-09 19:38:39 +0200

    lab1_2 added tests

--- labs/lab2_1/__init__.py
+++ labs/lab2_1/__init__.py
@@ -11,3 +11,21 @@ def exists():  # the name of the check
 def test1():
     """Does the program print D for a score of 60? """
     check50.run("python3 lab2_1.py").stdin("60").stdout("D\n").exit(0)
+
+
+@check50.check(exists)  # only run this check if the exists check has passed
+def test2():
+    """Does the program print A for a score of 92? """
+    check50.run("python3 lab2_1.py").stdin("92").stdout("A\n").exit(0)
+
+   
+@check50.check(exists)
+def test3():
+    """Does the program print 'Invalid input' for a score of 102? """
+    check50.run("python3 lab2_1.py").stdin("102").stdout("Invalid input\n").exit(0)
+
+
+def test4():
+    """Does the program print 'Invalid input' for a score of aa? """
+    check50.run("python3 lab2_1.py").stdin(
+        "aa").stdout("Invalid input\n").exit(0)
