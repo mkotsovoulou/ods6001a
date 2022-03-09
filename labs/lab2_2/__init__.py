6d3dfc5439ef8a5dcc4087164d9c0c199d257ad1
Maira Kotsovoulou <mkotsovoulou@acg.edu>
2022-03-09 20:07:33 +0200

    lab2_2 added tests

--- labs/lab2_2/__init__.py
+++ labs/lab2_2/__init__.py
@@ -8,24 +8,24 @@ def exists():  # the name of the check
 
 
 @check50.check(exists)  # only run this check if the exists check has passed
-def test1():
-    """Does the program print 25 for 5 inputs (5,5,5,5,5) """
-    check50.run("python3 lab2_2.py").stdin("5").stdin("5").stdin("5").stdin("5").stdin("5").stdout("25.0\n").exit(0)
-
-
-def test2():
-    """Does the program print 25 for 5 inputs (5,5,5,5,5) """
-    check50.run("python3 lab2_2.py").stdin("2").stdin("1").stdin(
+def test1(exists):
+    """Does the program print 0.0 for 2 inputs (1,-1) """
+    check50.run("python3 lab2_2.py").stdin("2").stdout("Type a number to add:").stdin("1").stdout("Type a number to add:").stdin(
         "-1").stdout("0.0\n").exit(0)
 
 @check50.check(exists)  # only run this check if the exists check has passed
 def test2():
+    """Does the program print 25 for 5 inputs (5,5,5,5,5) """
+    check50.run("python3 lab2_2.py").stdin("5").stdin("5").stdin("5").stdin("5").stdin("5").stdout("25.0\n").exit(0)
+
+@check50.check(exists)  # only run this check if the exists check has passed
+def test3():
     """Does the program print 'Wrong input' for a? """
     check50.run("python3 lab2_2.py").stdin("a").stdout("Wrong input\n").exit(0)
 
 
 @check50.check(exists)  # only run this check if the exists check has passed
-def test3():
+def test4():
     """Does the program print 'Wrong input' for 2 inputs (1,a)? """
     check50.run("python3 lab2_2.py").stdin("2").stdin(
         "a").stdout("Wrong input\n").exit(0)
