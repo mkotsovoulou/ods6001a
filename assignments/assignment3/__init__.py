4cfef8938e1ff15567f9a8ed9e2e8450fe3f4632
Maira Kotsovoulou <mkotsovoulou@acg.edu>
2022-03-09 21:34:57 +0200

    lab2_2 added tests

--- assignments/assignment3/__init__.py
+++ assignments/assignment3/__init__.py
@@ -7,14 +7,21 @@ def exists():  # the name of the check
 
 
 @check50.check(exists)  # only run this check if the exists check has passed
-def test1():
+def test1(exists):
     """Does the program calculate the area of a triangle with base 12 and height 45 correctly ? """
     check50.run(
         "python -c \"from assignment3 import areaTriangle;print(areaTriangle(12, 45))\"").stdout("270.0\n").exit(0)
 
 
 @check50.check(exists)  # only run this check if the exists check has passed
-def test2():
-    """Does the program call the function with arguments ? """
+def test2(exists):
+    """Does the program call the function with arguments and return correct result ? """
     check50.run(
-        "python assignment3.py").stdin("12").stdin("44").stdout("270.0\n").exit(0)
+        "python assignment3.py").stdin("12").stdin("45").stdout("270.0\n").exit(0)
+
+
+@check50.check(exists)  # only run this check if the exists check has passed
+def test2(exists):
+    """Does the program handle wrong input ? """
+    check50.run(
+        "python assignment3.py").stdin("a").stdout("^[Ww]rong [Ii]nput\.?\n").exit(0)
