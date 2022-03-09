3d448e0e17ea715baa6a54b9140f6ea9669ef053
Maira Kotsovoulou <mkotsovoulou@acg.edu>
2022-03-09 21:30:38 +0200

    lab2_2 added tests

--- assignments/assignment3/__init__.py
+++ assignments/assignment3/__init__.py
@@ -11,3 +11,10 @@ def test1():
     """Does the program calculate the area of a triangle with base 12 and height 45 correctly ? """
     check50.run(
         "python -c \"from assignment3 import areaTriangle;print(areaTriangle(12, 45))\"").stdout("270.0\n").exit(0)
+
+
+@check50.check(exists)  # only run this check if the exists check has passed
+def test2():
+    """Does the program call the function with arguments ? """
+    check50.run(
+        "python assignment3.py").stdin("12").stdin("45").stdout("270.0\n").exit(0)
