a785e75facbb1865fe6eba9daa4f488a8f796888
Maira Kotsovoulou <mkotsovoulou@acg.edu>
2022-03-16 20:57:52 +0200

    lab3_2

--- labs/lab3_2/lab3_2.py
+++ labs/lab3_2/lab3_2.py
@@ -1,13 +1,13 @@
 # the PSEUDOCODE of the solution of this program is as follows:
 
 # Open the datafile.txt
-fileh = open("datafile.txt")
+
 
 # Read the file contents into a string variable
-data = fileh.read()
+
 
 # Split the file into 'lines' for processing
-lines = data.splitlines()
+
 
 # Initialize a sum variable to 0
 
