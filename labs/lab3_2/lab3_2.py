6471fa6e9973b8f00418883ede6c3fe39f628822
Maira Kotsovoulou <mkotsovoulou@acg.edu>
2022-03-16 20:56:16 +0200

    lab3_2

--- labs/lab3_2/lab3_2.py
+++ labs/lab3_2/lab3_2.py
@@ -1,2 +1,21 @@
+# the PSEUDOCODE of the solution of this program is as follows:
+
+# Open the datafile.txt
+fileh = open("datafile.txt")
+
+# Read the file contents into a string variable
+data = fileh.read()
+
+# Split the file into 'lines' for processing
+lines = data.splitlines()
+
+# Initialize a sum variable to 0
+
+# Loop through each line in the collection of 'lines' retrieved from the file
+    # if the line contains xmax
+        # extract the number 
+        # add the number to the sum
+
+# print the sum
 
 
