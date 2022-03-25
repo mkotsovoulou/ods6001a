74f6932c152f6808247ee6bb1964a084ffd16e6e
Maira Kotsovoulou <mkotsovoulou@acg.edu>
2022-03-26 00:07:31 +0200

    lab4_1 added

--- labs/lab4_1/__init__.py
+++ labs/lab4_1/__init__.py
@@ -9,7 +9,9 @@ def exists():  # the name of the check
 @check50.check(exists)
 def main_test(exists):
     """Does the program read the file and count the unique words ?"""
-    check50.run("python3 lab4_1.py").stdout("26\n").exit(0)
+    output = check50.run("python3 lab4_1.py").stdout()
+    if output != ['Arise', 'But', 'It', 'Juliet', 'Who', 'already', 'and', 'breaks', 'east', 'envious', 'fair', 'grief', 'is', 'kill', 'light', 'moon', 'pale', 'sick', 'soft', 'sun', 'the', 'through', 'what', 'window', 'with', 'yonder']
+        raise check50.Failure("Correct output not detected")
 
 
 
