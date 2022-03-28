c1b9623a8720be7c98eec291aa692a99ec892055
Maira Kotsovoulou <mkotsovoulou@acg.edu>
2022-03-28 21:36:52 +0300

    lab4.2 corona

--- labs/lab4_2/lab4_2.py
+++ labs/lab4_2/lab4_2.py
@@ -1,5 +1,6 @@
 # Open the file to read it
-fh = open("2021-07-08_clean-hashtags.tsv")
-coronavirustags = dict()
-for line in fh:
-    print(line.rstrip())
+fhand = open('2021-07-08_clean-hashtags.tsv', 'r')
+
+virustags = {}
+
+
