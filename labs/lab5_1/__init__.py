6e2b6b34c79a67ebb9b722cbfbb7418b2ebc4717
Maira Kotsovoulou <mkotsovoulou@acg.edu>
2022-04-12 20:25:28 +0300

    lab5_1

--- labs/lab5_1/__init__.py
+++ labs/lab5_1/__init__.py
@@ -2,13 +2,16 @@ import check50  # import the check50 module
 
 @check50.check()  # tag the function below as check50 check
 def exists():  # the name of the check
-    """Is your file saved as lab6_1.py?"""  # this is what you will see when running check50
-    check50.exists("lab6_1.py")  # the actual check
+    """Is your file saved as lab5_1.py?"""  # this is what you will see when running check50
+    check50.exists("lab5_1.py")  # the actual check
+
+    """Does article1.txt exist?"""  # this is what you will see when running check50
+    check50.exists("article1.py")  # the actual check
 
 # only run this check if the exists check has passed
 @check50.check(exists)
 def main_test(exists):
-    """Does the program read the file and count tags ?"""
-    output = check50.run("python3 lab6_1.py").stdout()
-    if output != "#covid19vaccine 6460\n":
-        raise check50.Failure("Your code did not produce the expected tag and count")
+    """Does the program read the file and count the letter with the highest frequency ?"""
+    output = check50.run("python3 lab5_1.py").stdout()
+    if output != "E 661\n":
+        raise check50.Failure("Your code did not produce the expected count...")
