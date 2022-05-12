201de773c52601b8dd2f48a314e55f9423975b41
Maira Kotsovoulou <mkotsovoulou@acg.edu>
2022-05-12 13:56:30 +0300

    assignment 8

--- assignments/assignment8/__init__.py
+++ assignments/assignment8/__init__.py
@@ -3,7 +3,7 @@ import check50  # import the check50 module
 @check50.check()  # tag the function below as check50 check
 def exists():  # the name of the check
     """Is your file saved as assignment7.py?"""  # this is what you will see when running check50
-    check50.exists("assignment7.py")  # the actual check
+    check50.exists("assignment8.py")  # the actual check
 
 @check50.check(exists)  # tag the function below as check50 check
 def stats():  # the name of the check
@@ -13,5 +13,5 @@ def stats():  # the name of the check
 @check50.check(stats)
 def main_test():
     """Does the program find the most expensive price ?"""
-    check50.run("python3 assignment7.py").stdout("1749\n").exit(0)
+    check50.run("python3 assignment8.py").stdout("1749\n").exit(0)
 
