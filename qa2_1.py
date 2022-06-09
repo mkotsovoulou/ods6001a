9d2847ced37c5479a84f67240338a12177b125dd
Maira Kotsovoulou <mkotsovoulou@acg.edu>
2022-06-09 20:25:20 +0300

    ass6 submission error

---
+++ qa2_1.py
@@ -0,0 +1,11 @@
+fileh = open("text.txt")
+string_sentence = fileh.read()
+print(string_sentence)
+
+sample="dsds,3232,dasdsad,32132,dsalfd"
+dd = sample.split(",")
+
+print(dd[3])
+
+for whatever in [54,"dads32",43,54,65]:
+    print(whatever)
