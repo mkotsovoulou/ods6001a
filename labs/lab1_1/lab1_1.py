064290fcd4de812562daaf37f5f167c0b9a50edf
Maira Kotsovoulou <mkotsovoulou@gmail.com>
2025-05-10 19:17:07 +0000

    replit changes

--- labs/lab1_1/lab1_1.py
+++ labs/lab1_1/lab1_1.py
@@ -1,11 +1,12 @@
 
 def main():
-  x = int(input("x: "))
-  y = int(input("y: "))
+  x = input("x: ")
+  y = input("y: ")
   sum = x + y
 
-  print(f"Sum: {float(sum) if isinstance(sum, float) else sum}")
+  print(f"Sum: {sum}")
 
 
+# This condition checks if the script/program is being run directly. If it is, the main function is called.
 if __name__ == "__main__":
   main()
